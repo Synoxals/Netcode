a0c5d18478a78d7a436c2698eb789c0d7aa6b6a9
Synoxals <536662@chrome.escg.ac.uk>
2023-02-07 13:22:16 +0000

    Restarted repository

---
+++ Netcode 3D/Assets/Scripts/NetworkManagerUI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
+
+public class NetworkManagerUI : MonoBehaviour
+{
+    [SerializeField] private Button serverBtn;
+    [SerializeField] private Button hostBtn;
+    [SerializeField] private Button clientBtn;
+    [SerializeField] private Button startRelay;
+
+    TestRelay testRelay;
+
+    private void Awake()
+    {
+        serverBtn.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartServer();
+        });
+        hostBtn.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartHost();
+        });
+        clientBtn.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartClient();
+        });
+        startRelay.onClick.AddListener(() => {
+            Debug.Log("hi");
+            testRelay.CreateRelay();
+        });
+
+    }
+}
